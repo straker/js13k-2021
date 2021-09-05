2de50d3c32c439be51c418d734cad4d2a022adc4
straker <2433219+straker@users.noreply.github.com>
2021-09-04 23:20:12 -0600

    add mover

--- src/managers/mover-manager.js
+++ src/managers/mover-manager.js
@@ -11,25 +11,25 @@ const moverManager = {
     on('gameTick', () => {
       movers.forEach(mover => {
 
-	// movers move every 2 game ticks
-	if (++mover.timer % 2 === 0) {
-	  const fromRow = mover.row - mover.dir.row;
-	  const fromCol = mover.col - mover.dir.col;
-	  const toRow = mover.row + mover.dir.row;
-	  const toCol = mover.col + mover.dir.col;
+        // movers move every 2 game ticks
+        if (++mover.timer % 2 === 0) {
+          const fromRow = mover.row - mover.dir.row;
+          const fromCol = mover.col - mover.dir.col;
+          const toRow = mover.row + mover.dir.row;
+          const toCol = mover.col + mover.dir.col;
 
-	  const from = grid.get({ row: fromRow, col: fromCol })[0];
-	  const to = grid.getByType({ row: toRow, col: toCol }, TYPES.BELT)[0];
+          const from = grid.get({ row: fromRow, col: fromCol })[0];
+          const to = grid.getByType({ row: toRow, col: toCol }, TYPES.BELT)[0];
 
-	  if (from?.component && to && !to.component) {
-	    componentManager.add({ row: toRow, col: toCol });
-	    from.component = false;
-	    mover.name = 'MOVER_END';
-	  }
-	}
-	else {
-	  mover.name = 'MOVER';
-	}
+          if (from?.component && to && !to.component) {
+            componentManager.add({ row: toRow, col: toCol });
+            from.component = false;
+            mover.name = 'MOVER_END';
+          }
+        }
+        else {
+          mover.name = 'MOVER';
+        }
       });
     });
   },
